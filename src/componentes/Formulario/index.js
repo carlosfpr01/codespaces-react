57b0c7d6bcd75fe7d4fa8e33728b17cc89f9440b
Carlos Felipe Ramos Pires <141787491+carlosfpr01@users.noreply.github.com>
2023-10-23 21:01:08 +0000

    Fiz o cabeçalho e o formulario

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,33 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+
+const Formulario = () => {
+
+    const patrulhas=[
+        'Poti',
+        'Mayo',
+        'Aguia',
+        'Gorila',
+        'Leopardo',
+        'Escorpião',
+        'matilha-3',
+        'matilha-2',
+        'matilha-1'
+    ]
+
+    return (
+        <section className='formulario'>
+            <form>
+                <h2>Preencha os dados requisitados para concluir a criação do perfil</h2>
+                <CampoTexto label="Nome" placeholder="Digite o seu nome..."/>
+                <CampoTexto label="Cargo" placeholder="Digite o seu idade..."/>
+                <CampoTexto label="Imagem" placeholder="Digite o sua imagem..."/>
+                <ListaSuspensa label="Patrulhas" itens={patrulhas}/>
+            </form>
+        </section>
+
+    )
+}
+
+export default Formulario
